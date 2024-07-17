b3cd08075fa26faf2b2a0eb7abdcde566237b873
MrGKanev <contact@mrgkanev.eu>
2024-07-17 11:58:14 +0300

    Using a single source of plugins

--- stageguard.php
+++ stageguard.php
@@ -14,6 +14,26 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
+// List of plugins to deactivate and prevent activation in staging environment.
+$plugins_to_handle = array(
+    'bunnycdn/bunnycdn.php',
+    'redis-cache/redis-cache.php',
+    'google-listings-and-ads/google-listings-and-ads.php',
+    'metorik-helper/metorik-helper.php',
+    'order-sync-with-zendesk-for-woocommerce/order-sync-with-zendesk-for-woocommerce.php',
+    'redis-object-cache/redis-object-cache.php',
+    'runcloud-hub/runcloud-hub.php',
+    'site-kit-by-google/site-kit-by-google.php',
+    'super-page-cache-for-cloudflare/super-page-cache-for-cloudflare.php',
+    'ups-woocommerce-shipping/ups-woocommerce-shipping.php',
+    'woocommerce-shipstation-integration/woocommerce-shipstation.php',
+    'wp-opcache/wp-opcache.php',
+    'headers-security-advanced-hsts-wp/headers-security-advanced-hsts-wp.php',
+    'wp-rocket/wp-rocket.php',
+    'tidio-live-chat/tidio-live-chat.php',
+    'litespeed-cache/litespeed-cache.php'
+);
+
 // Add an admin notice indicating this is a staging environment.
 function staging_env_notice()
 {
@@ -24,22 +44,8 @@ add_action('admin_notices', 'staging_env_notice');
 // Deactivate specific plugins in staging environment.
 function deactivate_staging_plugins()
 {
-    $plugins_to_deactivate = array(
-        'bunnycdn/bunnycdn.php',
-        'redis-cache/redis-cache.php',
-        'google-listings-and-ads/google-listings-and-ads.php',
-        'metorik-helper/metorik-helper.php',
-        'order-sync-with-zendesk-for-woocommerce/order-sync-with-zendesk-for-woocommerce.php',
-        'redis-object-cache/redis-object-cache.php',
-        'runcloud-hub/runcloud-hub.php',
-        'site-kit-by-google/site-kit-by-google.php',
-        'super-page-cache-for-cloudflare/super-page-cache-for-cloudflare.php',
-        'ups-woocommerce-shipping/ups-woocommerce-shipping.php',
-        'woocommerce-shipstation-integration/woocommerce-shipstation.php',
-        'wp-opcache/wp-opcache.php'
-    );
-
-    foreach ($plugins_to_deactivate as $plugin) {
+    global $plugins_to_handle;
+    foreach ($plugins_to_handle as $plugin) {
         if (is_plugin_active($plugin)) {
             deactivate_plugins($plugin);
         }
@@ -59,22 +65,8 @@ add_action('admin_notices', 'stageguard_activation_notice');
 // Prevent activation of specific plugins with a custom error message.
 function prevent_plugin_activation($plugin)
 {
-    $plugins_to_deactivate = array(
-        'bunnycdn/bunnycdn.php',
-        'redis-cache/redis-cache.php',
-        'google-listings-and-ads/google-listings-and-ads.php',
-        'metorik-helper/metorik-helper.php',
-        'order-sync-with-zendesk-for-woocommerce/order-sync-with-zendesk-for-woocommerce.php',
-        'redis-object-cache/redis-object-cache.php',
-        'runcloud-hub/runcloud-hub.php',
-        'site-kit-by-google/site-kit-by-google.php',
-        'super-page-cache-for-cloudflare/super-page-cache-for-cloudflare.php',
-        'ups-woocommerce-shipping/ups-woocommerce-shipping.php',
-        'woocommerce-shipstation-integration/woocommerce-shipstation.php',
-        'wp-opcache/wp-opcache.php'
-    );
-
-    if (in_array($plugin, $plugins_to_deactivate)) {
+    global $plugins_to_handle;
+    if (in_array($plugin, $plugins_to_handle)) {
         deactivate_plugins($plugin);
         wp_safe_redirect(admin_url('plugins.php?stageguard_activation_error=true'));
         exit;
